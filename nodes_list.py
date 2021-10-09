0db53ccebd60d9db80f56568246702283aa5cd61
Jessie Cai <jcai8203@gmail.com>
2021-10-09 14:55:22 -0600

    added script and output file for nodes csv

---
+++ nodes_list.py
@@ -0,0 +1,22 @@
+#Create the .csv nodes list for use in Gephi
+#.csv in format "ID;Label;Category"
+import pandas as pd
+from pathlib import Path
+
+REDDIT_ROOT_PATH = Path(__file__).parent.absolute()
+USERS_DATA_PATH = "{}/data/user_data.txt".format(REDDIT_ROOT_PATH)
+NODES_CSV_PATH = "{}/data/subreddit_nodes.csv".format(REDDIT_ROOT_PATH)
+
+#Get subreddit names
+df = pd.read_csv(USERS_DATA_PATH, sep='\t')
+subreddits = list(df.columns.values)[2:]
+
+#Write to .csv nodes file
+file = pd.DataFrame()
+index = 0
+
+for subreddit in subreddits:
+    file = file.append({'Id':str(index), 'Label': subreddit, 'Category':''}, ignore_index=True)
+    index+=1
+
+file.to_csv(NODES_CSV_PATH, sep=';', index=False, encoding='utf-8')
